cb5237dc356ea5d798797723f1ede17d5bcbc3e9
jdavid42mh93 <jdavid42mh93@gmail.com>
2026-04-25 11:36:59 -0500

    refactor: Validate parameters and queries

--- main.py
+++ main.py
@@ -4,11 +4,54 @@ from pydantic import BaseModel, Field, field_validator
 
 app = FastAPI(title="My FastAPI Application")
 
-BLOG_POSTS = [
-    {"id": 1, "title": "Hello FastAPI", "content": "This is my first blog post."},
-    {"id": 2, "title": "FastAPI is Awesome", "content": "FastAPI makes it easy to build APIs."},
-    {"id": 3, "title": "Python is Great", "content": "Python is a versatile programming language."}
+BLOG_POST = [
+    {"id": 1, "title": "Hello from FastAPI",
+        "content": "My first post with FastAPI"},
+    {"id": 2, "title": "My second post with FastAPI",
+        "content": "My second post with FastAPI blah blah"},
+    {"id": 3, "title": "Django vs FastAPI",
+        "content": "FastAPI is faster for x reasons",
+        "tags": [
+            {"name": "Python"},
+            {"name": "fastapi"},
+            {"name": "Django"}
+        ]},
+    {"id": 4, "title": "Hello from FastAPI",
+        "content": "My first post with FastAPI"},
+    {"id": 5, "title": "My second post with FastAPI",
+        "content": "My second post with FastAPI blah blah"},
+    {"id": 6, "title": "Django vs FastAPI",
+        "content": "FastAPI is faster for x reasons"},
+    {"id": 7, "title": "Hello from FastAPI",
+        "content": "My first post with FastAPI"},
+    {"id": 8, "title": "My second post with FastAPI",
+        "content": "My second post with FastAPI blah blah"},
+    {"id": 9, "title": "Django vs FastAPI",
+        "content": "FastAPI is faster for x reasons"},
+    {"id": 10, "title": "Hello from FastAPI",
+        "content": "My first post with FastAPI"},
+    {"id": 11, "title": "My second post with FastAPI",
+        "content": "My second post with FastAPI blah blah"},
+    {"id": 12, "title": "Django vs FastAPI",
+        "content": "FastAPI is faster for x reasons",
+        "tags": [
+            {"name": "Python"},
+            {"name": "fastapi"},
+            {"name": "Django"}
+        ]},
+    {"id": 13, "title": "Hello from FastAPI",
+        "content": "My first post with FastAPI"},
+    {"id": 14, "title": "My second post with FastAPI",
+        "content": "My second post with FastAPI blah blah"},
+    {"id": 15, "title": "Django vs FastAPI",
+        "content": "FastAPI is faster for x reasons",
+        "tags": [
+            {"name": "Python"},
+            {"name": "fastapi"},
+            {"name": "Django"}
+        ]},
 ]
+
 
 SPAWM_WORDS = ["spam", "junk", "scam", "fake"]
 
@@ -54,26 +97,65 @@ class PostSummary(BaseModel):
     id: int
     title: str
 
-@app.get("/posts", response_model=List[PostPublic])
-def list_posts(query: Optional[str] = Query(
+class PostPaginated(BaseModel):
+    page: int
+    per_page: int
+    total: int
+    total_pages: int
+    has_prev: bool
+    has_next: bool
+    order_by: Literal["id", "title"]
+    direction: Literal["asc", "desc"]
+    search: Optional[str] = None
+    limit: int
+    offset: int
+    items: List[PostPublic]
+
+@app.get("/posts", response_model=PostPaginated, response_description="A paginated list of blog posts")
+def list_posts(
+    text: Optional[str] = Query(
+        default=None,
+        deprecated=True,
+        description="Deprecated parameter, use 'query or search' instead."
+    ),
+    query: Optional[str] = Query(
         default=None,
         description="Search query for blog posts",
-        alias="serach",
+        alias="search",
         min_length=3,
         max_length=50,
         example="FastAPI"
     ), 
-    limit: int = Query(10, ge=1, le=50, description="Number of posts to return"),
-    offset: int = Query(0, ge=0, description="Number of posts to skip"),
+    per_page: int = Query(10, ge=1, le=50, description="Number of posts to return"),
+    page: int = Query(1, ge=1, description="Page number"),
     order_by: Literal["id", "title"] = Query("id", description="Sort order of the posts"),
     direction: Literal["asc", "desc"] = Query("asc", description="Sort direction of the posts")
     ):
-    results = BLOG_POSTS
+    results = BLOG_POST
     if query:
         results = [post for post in results if query.lower() in post["title"].lower()]
+    total = len(results)
+    offset = (page - 1) * per_page
+    items=results[offset: offset + per_page]
+    total_pages = (total + per_page - 1) // per_page
+    has_prev = offset > 0
+    has_next = offset + per_page < total
     results = sorted(results, key=lambda post: post[order_by], reverse=(direction == "desc"))
-    return results[offset: offset + limit]
-    
+    return PostPaginated(
+        page=offset // per_page + 1,
+        per_page=per_page,
+        total=total,
+        total_pages=total_pages,
+        has_prev=has_prev,
+        has_next=has_next,
+        order_by=order_by,
+        direction=direction,
+        search=query,
+        limit=per_page,
+        offset=offset,
+        items=items
+    )
+
 @app.get("/posts/{post_id}", response_model=Union[PostPublic, PostSummary], response_description="Returns either the full post or a summary based on the 'include_content' query parameter")
 def get_post(post_id: int = Path(..., ge=1, title="The ID of the post to retrieve", description="The ID of the post to retrieve", example=1), include_content: Optional[bool] = Query(default=True, description="Include content in the response")):
     for post in BLOG_POSTS:
@@ -82,6 +164,20 @@ def get_post(post_id: int = Path(..., ge=1, title="The ID of the post to retriev
                 return {"id": post["id"], "title": post["title"]}
             return post
     raise HTTPException(status_code=404, detail="Post not found")
+
+@app.get("/posts/by-tags", response_model=List[PostPublic])
+def filter_by_tags(
+    tags: List[str] = Query(
+        ...,
+        min_length=2,
+        description="Una o más etiquetas. Ejemplo: ?tags=python&tags=fastapi"
+    )
+):
+    tags_lower = [tag.lower() for tag in tags]
+
+    return [
+        post for post in BLOG_POST if any(tag["name"].lower() in tags_lower for tag in post.get("tags", []))
+    ]
 
 @app.post("/posts", response_model=PostPublic, response_description="The created blog post")
 def create_post(post: PostCreate):
@@ -108,8 +204,8 @@ def update_post(post_id: int, data: PostUpdate):
 
 @app.delete("/posts/{post_id}", status_code=204)
 def delete_post(post_id: int):
-    for index, post in enumerate(BLOG_POSTS):
+    for index, post in enumerate(BLOG_POST):
         if post["id"] == post_id:
-            BLOG_POSTS.pop(index)
+            BLOG_POST.pop(index)
             return
     raise HTTPException(status_code=404, detail="Post not found")
